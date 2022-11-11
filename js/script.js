8231ab1462488b92a5ba5d13578c01f52c48ecfe
Stian Marton <stianmarton2@gmail.com>
2022-11-11 21:02:21 +0100

    Game can be played five times but is buggy

--- js/script.js
+++ js/script.js
@@ -1,86 +1,67 @@
 
 /* Rock-paper-scissor in Javascript */
 
-/*Make a variable that contains the strings "rock, paper and scissor"*/
-
-var hand = ["rock", "paper", "scissor"];
-
-/*The computer returns rock, paper or scissor at random */
-
-function getComputerChoice (hand) {
-    return hand[Math.floor(Math.random()*hand.length)];
-}
-
-let computerSelection = getComputerChoice(hand);
-
-/*- A prompt gets the input from user, and stores it in a variable.
-  - The parameter must be case-insensitive
-  - When user selects a hand the computer also selects a hand*/
-
-let userSelection = prompt('Please type "rock, paper or scissor".');
-
-if (userSelection.toLowerCase() === "rock" || userSelection.toLocaleLowerCase() === "paper" || userSelection.toLocaleLowerCase() === "scissor") {
-        console.log(`You select ${userSelection}`);
-        console.log(`The computer selects ${computerSelection}`);
-    } else {
-        console.log('Please type "rock, paper or scissor"');
-    }
+/* The game can now be replayed five times.
+Fix the winner message when the game is over
+Fix the timer so it don't count when players don't type "rock, paper or scissor" correctly */
 
 
-/* OLD CHECKHAND
-function checkHand (userSelection) {
-    userSelection = userSelection.toLowerCase();
-    if (userSelection == "rock" || userSelection == "paper" || userSelection == "scissor") {
-        checkHand = userSelection;
-        console.log(`You select ${checkHand}`);
-        console.log(`The computer selects ${computerSelection}`);
-        return userSelection;
-    } else {
-        console.log('Please type "rock, paper or scissor"');
-    }
-}
-
-let userSelection = checkHand('paper');
-*/
-
-/*A single round, user against computer*/
-
+/*Declare the scores*/
 let userScore = 0;
 let computerScore = 0;
 
-function playRound (userSelection, computerSelection) {
-    if (userSelection == "rock" && computerSelection == "scissor"
-        || userSelection == "paper" && computerSelection == "rock"
-        || userSelection == "scissor" && computerSelection == "paper")  {
-        userScore++;
-        return `You win, ${userSelection} beats ${computerSelection}.`;
-    } else if (userSelection == computerSelection) {
-        return "Draw! Do it again.";
-    } else if (userSelection == "rock" && computerSelection == "paper"
-        || userSelection == "paper" && computerSelection == "scissor"
-        || userSelection == "scissor" && computerSelection == "rock"){
-        computerScore++
-        return `You lose, ${computerSelection} beats ${userSelection}.`;
-    } else {
-       return "No game";
+/* This function runs the entire game up to five times*/ 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        if (i === 5 && computerScore > userScore) {
+            console.log("Game is over, computer wins the game!");
+        } else if (i === 5 && userScore < computerScore) {
+            console.log("Game is over, you win the game!");
+        } else {
+
+            /*Make a variable that contains the strings "rock, paper and scissor"*/
+            var hand = ["rock", "paper", "scissor"];
+
+            /*The computer returns rock, paper or scissor at random */
+            function getComputerChoice (hand) {
+                return hand[Math.floor(Math.random()*hand.length)];
+            }
+            let computerSelection = getComputerChoice(hand);
+
+            /*- A prompt gets the input from user, and stores it in a variable.
+            - The parameter must be case-insensitive
+            - When user selects a hand the computer also selects a hand*/
+            let userSelection = prompt('Please type "rock, paper or scissor".');
+
+                if (userSelection.toLowerCase() === "rock" || userSelection.toLocaleLowerCase() === "paper" || userSelection.toLocaleLowerCase() === "scissor") {
+                    console.log(`You select ${userSelection}`);
+                    console.log(`The computer selects ${computerSelection}`);
+                } else {
+                    console.log('Please type "rock, paper or scissor"');
+                }
+
+
+            /*A single round, user against computer*/
+            function playRound (userSelection, computerSelection) {
+                if (userSelection == "rock" && computerSelection == "scissor"
+                    || userSelection == "paper" && computerSelection == "rock"
+                    || userSelection == "scissor" && computerSelection == "paper") {
+                    userScore++;
+                    return `You win this round, ${userSelection} beats ${computerSelection}.`;
+                } else if (userSelection == computerSelection) {
+                    return "Draw! Do it again.";
+                } else if (userSelection == "rock" && computerSelection == "paper"
+                    || userSelection == "paper" && computerSelection == "scissor"
+                    || userSelection == "scissor" && computerSelection == "rock") {
+                    computerScore++
+                    return `You lose this round, ${computerSelection} beats ${userSelection}.`;
+                } else {
+                    return "No game";
+                }
+            }
+                console.log(playRound(userSelection, computerSelection));
+                console.log(`Score is ${userScore} to ${computerScore} at round ${i}`);         
+        } 
     }
 }
-
-console.log(playRound(userSelection, computerSelection));
-
-/* a game of 5
-Keeps score of each round
-Reports a winner or loser at the end */
-
-function game(playRound) {
-    for (playRound(userScore, computerScore); userScore < 5 || computerScore < 5;)
-        if (userScore === 5) {
-            return"User wins";
-        } else if (computerScore === 5) {
-            return"Computer wins";
-        } else {
-            return `Score is ${userScore} to ${computerScore}`;
-        }
-    }
-
-console.log(game(playRound));
+console.log(game());
